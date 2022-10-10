c911514e630493ae4872a7667e21ab1cab3e6f6f
Nikolay Bryskin <nbryskin@gmail.com>
2022-10-10 16:07:03 +0300

    [build] Fix building for AMO

--- scripts/download-tar-for-amo.sh
+++ scripts/download-tar-for-amo.sh
@@ -1,3 +1,9 @@
 #!/bin/sh -e
 
-wget -O ~/Downloads/donate4fun.tar.gz https://github.com/donate4fun/donate4fun/tarball/master
+tmpfile=$(mktemp)
+tmpdir=$(mktemp -d)
+wget -O $tmpfile https://github.com/donate4fun/donate4fun/tarball/master
+tar -C $tmpdir -zxf $tmpfile
+rm $tmpfile
+tar -C $tmpdir --dereference --hard-dereference -zcf ~/Downloads/donate4fun.src.tar.gz .
+rm -r $tmpdir
