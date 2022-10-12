ce183e2d4aa60480c1cb4ed5a0611a7d4b45dcc8
Nikolay Bryskin <nbryskin@gmail.com>
2022-10-12 13:40:31 +0300

    [extension] Redesign Popup corners

--- extensions/src/PopupSection.svelte
+++ extensions/src/PopupSection.svelte
@@ -7,7 +7,8 @@
 <style>
 section {
   background-image: linear-gradient(to right, #F9F03E 0%, #9DEDA2 100%);
-  border-radius: 20px;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
   padding: 2px;
   height: 100%;
 }
@@ -15,7 +16,8 @@ section > div {
   width: 100%;
   height: 100%;
   background: white;
-  border-radius: 20px;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
   padding: 40px 32px 36px 32px;
   align-items: var(--align-items);
   justify-content: var(--justify-content);
