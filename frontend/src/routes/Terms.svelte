4d6ffda9f6dbc437b33e0b04e3f0313d028eaec5
Nikolay Bryskin <nbryskin@gmail.com>
2022-10-12 13:28:56 +0300

    [frontend] Update Terms page

--- frontend/src/routes/Terms.svelte
+++ frontend/src/routes/Terms.svelte
@@ -1,4 +1,157 @@
-<h1>Terms of use</h1>
-Donate4.Fun is a Bitcoin Lightning "locker" service - donater locks some amount of satoshis that can be later withdrawn by content creator.
-Funds withdrawal is solely author (e.g. Youtube Channel) owner responsibility.
-Donations could be refunded to donator account if donatee do not link his channel in 3 months after the donation.
+<script>
+  import Section from "$lib/Section.svelte";
+</script>
+
+<div>
+  <Section --padding="20px 40px">
+  <h1>Terms of service</h1>
+  Last updated on October 10, 2022
+
+  <section>
+  Please read these terms of service (the "Terms") carefully before using our website. By accessing or using any part of the site, you agree to be bound by these Terms. If you do not agree to all the terms and conditions herein, then you may not access the website or use any Donate4.Fun services.
+  </section>
+
+  <section>
+  <h1>OVERVIEW</h1>
+
+  These Terms constitute a legal agreement between us (the "Donate4.Fun") and users, clients, visitors and others (without limitation), who access the Website (as defined below) (the "User", "You", "Your"). Donate4.Fun offers the set of related web pages and services located under a single domain named fixedfloat.com (the "Website", the "Service"), including all information, tools and services available from this Site to you, given upon your acceptance of all terms, conditions, policies, and notices stated here.
+  </section>
+  <section>
+  <h1>DONATE4.FUN TERMS</h1>
+
+  You hereby confirm that you are an individual, legal person or other organisation with full capacity for civil rights and civil conducts when you use the Service. By agreeing to these Terms, you represent that you are at least the age of majority in your state or province of residence or you have consent from your legal representative or guardian to use the Service.
+
+  You may not use our services and products for any illegal or unauthorized purpose nor may you, in the use of the Service, violate any laws in any applicable jurisdiction (including but not limited to copyright laws).
+
+  A breach or violation of any of the Terms will result in an immediate termination of the services provided to you.
+  </section>
+  <section>
+  <h1>GENERAL CONDITIONS</h1>
+
+  The headings used in this agreement are included for convenience only and will not limit or otherwise affect these Terms.
+
+  We reserve the right to refuse service to anyone for any reason at any time.
+
+  You understand that your content, may be transferred unencrypted and involve (a) transmissions over various networks; and (b) changes to conform and adapt to technical requirements of connecting networks or devices.
+
+  You agree not to reproduce, duplicate, copy, sell, resell or exploit any portion of the Service, use of the Service, or access to the Service or any contact on the Website through which the service is provided, without express written permission by us.
+
+  Certain personal information provided to us is protected according to the Privacy Policy.
+  </section>
+  <section>
+  <h1>USER COMMENTS, FEEDBACK AND OTHER SUBMISSIONS</h1>
+
+  If, at our request, you send certain specific submissions or without a request from us you send creative ideas, suggestions, proposals, plans, or other materials, whether online, by email, by postal mail, or otherwise (collectively, 'comments'), you agree that we may, at any time, without restriction, edit, copy, publish, distribute, translate and otherwise use in any media any comments that you forward to us.
+  </section>
+  <section>
+  <h1>PROHIBITED USES</h1>
+
+  In addition to other prohibitions as set forth in these Terms, you are prohibited from using the Website or its content: (a) for any unlawful purpose; (b) to solicit others to perform or participate in any unlawful acts; (c) to violate any international, federal, provincial or state regulations, rules, laws, or local ordinances; (d) to infringe upon or violate our intellectual property rights or the intellectual property rights of others; (e) to harass, abuse, insult, harm, defame, slander, disparage, intimidate, or discriminate based on gender, sexual orientation, religion, ethnicity, race, age, national origin, or disability; (f) to submit false or misleading information; (g) to upload or transmit viruses or any other type of malicious code that will or may be used in any way that will affect the functionality or operation of the Service or of any related website, other websites, or the Internet; (h) to collect or track the personal information of others; (i) to spam, phish, pharm, pretext, spider, crawl, or scrape; (j) for any obscene or immoral purpose; or (k) to interfere with or circumvent the security features of the Service or any related website, other websites, or the Internet. If fraudulent or stolen funds are sent to our addresses, the funds may be frozen until information about the source and legality of the funds is received. We reserve the right to terminate your use of the Service or any related website for violating any of the prohibited uses.
+  </section>
+  <section>
+  <h1>DISCLAIMER OF WARRANTIES; LIMITATION OF LIABILITY</h1>
+
+  We do not guarantee, represent or warrant that your use of our services will be uninterrupted, timely, secure or error-free.
+
+  You agree that from time to time we may remove the service for indefinite periods of time or cancel the service at any time, without notice to you.
+
+  In no case shall Donate4.Fun be liable for lost profits, lost revenue, lost savings, loss of data, replacement costs, or any similar damages.
+
+  THE WEBSITE AND THE SERVICES ARE PROVIDED ON AN "AS IS" BASIS AND WITHOUT ANY WARRANTIES OR INDEMNITIES OF ANY KIND, EITHER EXPRESSED OR IMPLIED. YOU ASSUME ALL RESPONSIBILITY AND RISK WITH RESPECT TO YOUR USE OF THE WEBSITE AND SERVICES. TO THE MAXIMUM EXTENT PERMITTED BY THE APPLICABLE LAWS, WE OR OUR AFFILIATES DO NOT ACCEPT ANY LIABILITY FOR ANY DAMAGE OR LOSS, INCLUDING LOSS OF BUSINESS, REVENUE, OR PROFITS, OR LOSS OF OR DAMAGE TO DATA, EQUIPMENT, OR SOFTWARE (DIRECT, INDIRECT, PUNITIVE, ACTUAL, CONSEQUENTIAL, INCIDENTAL, SPECIAL, EXEMPLARY OR OTHERWISE), RESULTING FROM ANY USE OF, OR INABILITY TO USE, THIS WEBSITE AND THE SERVICES OR THE MATERIAL, INFORMATION, SOFTWARE, FACILITIES, SERVICES OR CONTENT ON THIS REGARDLESS OF THE BASIS, UPON WHICH THE LIABILITY IS CLAIMED.
+
+  IF APPLICABLE LAWS DO NOT ALLOW ALL OR ANY PART OF THE ABOVE LIMITATION OF LIABILITY TO APPLY TO YOU, THE LIMITATIONS WILL APPLY TO YOU ONLY TO THE EXTENT PERMITTED BY APPLICABLE LAWS. YOU UNDERSTAND AND AGREE THAT IT IS YOUR OBLIGATION TO ENSURE COMPLIANCE WITH ANY LEGISLATION RELEVANT TO YOUR COUNTRY OF DOMICILE CONCERNING USE OF THE WEBSITE, AND THAT YOU SHOULD NOT ACCEPT ANY LIABILITY FOR ANY ILLEGAL OR UNAUTHORIZED USE OF THE WEBSITE. YOU AGREE TO BE SOLELY RESPONSIBLE FOR ANY APPLICABLE TAXES IMPOSED UNDER YOUR TAX RESIDENCY REGULATIONS.
+
+  IN NO EVENT SHALL WE, OUR DIRECTORS, MEMBERS, EMPLOYEES OR AGENTS BE LIABLE FOR ANY LOSS RESULTING FROM HACKING, TAMPERING, VIRUS TRANSMISSION OR OTHER UNAUTHORIZED ACCESS OR USE OF THE SERVICES, YOUR ACCOUNT, OR ANY INFORMATION CONTAINED THEREIN; FOR PRODUCTS NOT BEING AVAILABLE FOR USE; FOR IMPROPER FUNCTIONALITY, TECHNICAL FAULTS AND DOWNTIME OF THE TECHNICAL INFRASTRUCTURE.
+
+  WE DO NOT GUARANTEE CONTINUOUS, UNINTERRUPTED OR SECURE ACCESS TO THE SERVICES AND YOU ACKNOWLEDGE AND AGREE THAT THE WEBSITE MAY BE INTERFERED WITH BY NUMEROUS FACTORS OUTSIDE OF OUR CONTROL.
+  </section>
+  <section>
+  <h1>INDEMNIFICATION</h1>
+
+  You hereby agree to indemnify the Donate4.Fun, any of its officers, directors, employees and agents and its affiliated and related entities from and against any claims, costs, losses, liabilities, damages, expenses and judgments of any and every kind arising out of, relating to, or incurred in connection with any claim, complaint, audit, inquiry, or other proceeding, that arises or relates to: (a) any actual or alleged breach of your representations, warranties, or obligations set forth in these Terms; (b) your wrongful or improper use of the services; (c) any other party’s access or use of the services with your data; (d) arising out of a breach of any warranty, representation, or obligation hereunder.
+
+  You shall not have any claim of any nature whatsoever against us for any failure to carry out any of our obligations under these Terms as a result of a force majeure - causes beyond our control, including but not limited to any strike, lockout, shortage of labor or materials, delays in transport, in securing any permit, consent or approval required by us, for the supply of products, delay by any sub-contractor or supplier of ours accidents of any kind, riot, political or civil disturbances, the elements, by an act of state or government including regulatory action imposed or any other authority or any other cause whatsoever beyond our absolute and direct control.
+  </section>
+  <section>
+  <h1>SEVERABILITY</h1>
+
+  In the event that any provision of these Terms is determined to be unlawful, void or unenforceable, such provision shall nonetheless be enforceable to the fullest extent permitted by applicable law, and the unenforceable portion shall be deemed to be severed from these Terms, such determination shall not affect the validity and enforceability of any other remaining provisions.
+  </section>
+  <section>
+  <h1>TERMINATION</h1>
+
+  The obligations and liabilities of the parties incurred prior to the termination date shall survive the termination of this agreement for all purposes.
+
+  These Terms are effective unless and until terminated by either you or us. You may terminate these Terms at any time by notifying us that you no longer wish to use our Services, or when you cease using our Website.
+  </section>
+  <section>
+  <h1>ENTIRE AGREEMENT</h1>
+
+  The failure of us to exercise or enforce any right or provision of these Terms shall not constitute a waiver of such right or provision.
+
+  These Terms and any policies or operating rules posted by us on the Website or in respect of the Service constitutes the entire agreement and understanding between you and us and govern your use of the Service, superseding any prior or contemporaneous agreements, communications and proposals, whether oral or written, between you and us (including, but not limited to, any prior versions of these Terms).
+
+  Any ambiguities in the interpretation of these Terms shall not be construed against the drafting party.
+  </section>
+  <section>
+  <h1>TAX</h1>
+
+  The taxation of digital assets has been limited to the most jurisdictions. You may be subject to certain tax implications when buying or selling digital assets.
+  </section>
+  <section>
+  <h1>CHANGES TO TERMS OF SERVICE</h1>
+
+  You can review the most current version of the Terms at any time at this page.
+
+  We reserve the right, at our sole discretion, to update, change or replace any part of these Terms by posting updates and changes to our website. It is your responsibility to check our website periodically for changes. Your continued use of or access to our website or the Service following the posting of any changes to these Terms constitutes acceptance of those changes.
+  </section>
+  <section>
+  <h1>RISK</h1>
+
+  You acknowledge and agree that you shall use the Services at your own risk. You should, therefore, carefully consider whether such exchange is suitable for you in light of your circumstances and financial resources.
+  </section>
+  <section>
+  <h1>FEES</h1>
+
+  You agree to pay us the fees for donations completed via our Services:
+
+  0% of the amount of withdrawed or donated funds.
+  </section>
+  <section>
+  <h1>INTELLECTUAL PROPERTY RIGHTS</h1>
+
+  The Donate4.Fun exclusively owns all rights, title, and interest in the "DONATE4.FUN" name, "DONATE4.FUN" logo, the names of individual services and their logos, patents, copyrights (including rights in derivative works), trademarks or service marks, logos and designs, trade secrets, and other intellectual property embodied by, or contained on the Website and relating webpages, documentation etc. You shall never use any DONATE4.FUN logos, marks or other intellectual property for commercial and public use without our express permission, unless otherwise explicitly indicated by the Donate4.Fun. You shall not copy, reproduce, distribute, modify, create derivative works of, publicly display, publicly perform, republish, download, store, transmit or otherwise use any of the Donate4.Fun logos, marks or intellectual property. You also shall not make any public statement or issue any press release related to us or our services without our express permission. Under these Terms you are entitled to use any logos or marks for your personal, non-commercial purposes exclusively.
+  </section>
+  <section>
+  <h1>RESTRICTED ACTIVITIES</h1>
+
+  In connection with your use of the Service, or in the course of your interactions with the Donate4.Fun, you shall not:
+
+      breach these Terms or any policy approved by the Donate4.Fun and agreed by you.
+      violate any law, statute, ordinance, or regulation.
+      infringe the Donate4.Fun or any third party's copyright, patent, trademark, exchange secret or other intellectual property rights, or rights of publicity or privacy.
+      act in a manner that is defamatory, trade libellous, threatening or harassing to our employees, agents or other users.
+      provide false, inaccurate or misleading Information.
+      engage in potentially fraudulent or suspicious activity and/or transactions
+  </section>
+  <section>
+  <h1>DISPUTES RESOLUTION</h1>
+
+  All disputes and disagreements that might arise from these Terms shall be resolved by means of negotiations. You agree that for the purposes of the settlement of disputes between you and us, an e-mail correspondence with the authorized persons of the Donate4.Fun at: info@fixedfloat.com shall be the effective and binding method of communication. If the parties cannot agree on the subject of the dispute within thirty (30) days, the dispute shall be shall be referred to and finally resolved by the relevant court. To the extent allowed by the applicable law, you agree that you will bring any claim arising from or connected with these Terms of use within one (1) year from the date of which such claim arose. Otherwise such claims will be irrevocably waived.
+  </section>
+  <section>
+  <h1>CONTACT INFORMATION</h1>
+
+  Questions about these Terms should be sent to us at support@donate4.fun.
+  </section>
+  </Section>
+</div>
+
+<style>
+div {
+  max-width: 640px;
+}
+section {
+  padding: 20px 0;
+}
+</style>
