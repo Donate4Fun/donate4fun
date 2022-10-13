63e5d87bdfcc7fd731afa3d8d72e5ddb8d3160b2
Nikolay Bryskin <nbryskin@gmail.com>
2022-10-13 13:46:56 +0300

    [frontend] Fix Terms link in footer

--- frontend/src/lib/Footer.svelte
+++ frontend/src/lib/Footer.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { link } from "svelte-navigator";
   import GithubBrand from "svelte-awesome-icons/GithubBrand.svelte";
   import DiscordBrand from "svelte-awesome-icons/DiscordBrand.svelte";
   import TwitterBrand from "svelte-awesome-icons/TwitterBrand.svelte";
@@ -10,7 +11,7 @@
     <a target=_blank href="https://twitter.com/donate4_fun"><TwitterBrand size=32 color="var(--link-color)" /></a>
     <a target=_blank href="https://github.com/donate4Fun/donate4fun"><GithubBrand size=32 color="var(--link-color)" /></a>
     <a target=_blank href="https://discord.gg/pfFRGWZ4TM"><DiscordBrand size=32 color="var(--link-color)" /></a>
-    <a href="/terms">Terms and Privacy</a>
+    <a use:link href="/terms">Terms and Privacy</a>
   </div>
 </footer>
 
