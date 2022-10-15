308587882436f74de64f9546724913963488882c
Nikolay Bryskin <nbryskin@gmail.com>
2022-10-15 12:02:02 +0300

    [frontend] Fix logo stroke

--- frontend/src/lib/Logo.svelte
+++ frontend/src/lib/Logo.svelte
@@ -3,26 +3,43 @@
     <img class=text src="/static/D.svg" alt="D" width=28>
   </div>
   <div class="full">
-    <div class="text">Donate4Fun</div>
+    <img src="/static/ln-icon.svg" alt="bolt" height=40>
+    <div class="stoke-text">
+      <svg class="back" height="1.5em" width=300>
+        <text class="stroke" x="0.1em" y="0.97em">
+          Donate4Fun
+        </text>
+      </svg>
+      <div class="text">Donate4Fun</div>
+    </div>
   </div>
 </div>
 
 <style>
 .full {
-  background-image: url("/static/logo-background.svg");
+  font-weight: 900;
+  font-size: 29px;
+  display: flex;
+  gap: 6px;
+  align-items: center;
 }
 .full .text {
-  padding-left: 31.63px;
-  padding-top: 4px;
-  padding-right: 3px;
-  font-weight: 900;
-  font-size: 26.5333px;
-  line-height: 32px;
   background: linear-gradient(90deg, #F9F03E 0%, #9DEDA2 100%), linear-gradient(299.9deg, #FF7A00 18.72%, #FFA800 82.21%);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
   background-clip: text;
   text-fill-color: transparent;
+  position: relative;
+  padding: 0 0.1em;
+}
+.back {
+  position: absolute;
+  z-index: -1;
+}
+.stroke {
+  stroke: #2E6CFF;
+  stroke-width: 8px;
+  fill: none;
 }
 @media only screen and (max-width: 640px) {
   .full {
