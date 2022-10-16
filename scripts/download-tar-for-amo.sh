59c6889e4c329ba428926323e0dae004bec155d3
Nikolay Bryskin <nbryskin@gmail.com>
2022-10-16 17:18:29 +0300

    [scripts] Allow to download AMO sources archive for a custom ref

--- scripts/download-tar-for-amo.sh
+++ scripts/download-tar-for-amo.sh
@@ -1,8 +1,10 @@
 #!/bin/sh -e
 
+ref=${1:-master}
+
 tmpfile=$(mktemp)
 tmpdir=$(mktemp -d)
-wget -O $tmpfile https://github.com/donate4fun/donate4fun/tarball/master
+wget -O $tmpfile https://github.com/donate4fun/donate4fun/tarball/$ref
 tar -C $tmpdir -zxf $tmpfile
 rm $tmpfile
 tar -C $tmpdir --dereference --hard-dereference -zcf ~/Downloads/donate4fun.src.tar.gz .
