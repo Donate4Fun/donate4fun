42aa6a7639314fa2ef4d9363045e0ea2c5ce16b7
Nikolay Bryskin <nbryskin@gmail.com>
2022-10-26 17:40:42 +0300

    [web] Fix favicon

--- scripts/make-icons.sh
+++ scripts/make-icons.sh
@@ -2,5 +2,5 @@
 
 cd frontend/public/static
 for size in 16 24 32 48 64 128 256 512; do
-  inkscape -w $size -h $size D.svg -o D-$size.png
+  inkscape -w $size -h $size D.svg -o D-$size.png -b transparent -y 0
 done
