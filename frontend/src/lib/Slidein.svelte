a29d00bb91bfcf3df6836f815cf01ab11e1e576e
Nikolay Bryskin <nbryskin@gmail.com>
2022-10-27 17:36:03 +0300

    WIP

--- frontend/src/lib/Slidein.svelte
+++ frontend/src/lib/Slidein.svelte
@@ -1,41 +1,68 @@
 <script>
   import { fade } from 'svelte/transition';
   import { inview } from 'svelte-inview';
+  import ScrollMagic from 'scrollmagic';
 
   import { cLog } from "$lib/log.js";
 
   let elem;
+  let animate = false;
+  const controller = new ScrollMagic.Controller()
 
-  function onChange({detail}) {
+  onMount(() => {
+    const scene = new ScrollMagic.Scene({
+      triggerHook: 0.9
+    })
+  });
+
+  function onChangeAnimate({detail}) {
+    if (!animate) {
+      cLog("no animation", elem);
+      return;
+    }
     const animation = elem.getAnimations()[0];
     if (detail.inView && detail.scrollDirection.vertical === 'up') {
-      cLog("up start")
+      cLog("forward", elem);
       animation.playbackRate = 1;
       animation.play();
     }
     if (!detail.inView && detail.scrollDirection.vertical === 'down') {
-      cLog("down start");
+      cLog("backward", elem);
       animation.playbackRate = -1;
       animation.play();
     }
   }
-  function onEnter() {
-    cLog("enter");
+
+  function onChangeInit({detail}) {
+    if (detail.inView && detail.scrollDirection.vertical === 'up') {
+      cLog("add class", elem);
+      animate = true;
+    }
+    if (!detail.inView && detail.scrollDirection.vertical === 'down') {
+      cLog("remove class", elem);
+      animate = false;
+    }
   }
 </script>
 
 <div
-  class="wrapper"
-  use:inview={{rootMargin: '-100px'}}
-  on:change={onChange}
-  on:enter={onEnter}
+  use:inview={{}}
+  on:change={onChangeInit}
 >
-  <div bind:this={elem} class="animate">
-    <slot />
+  <div
+    use:inview={{rootMargin: '-100px'}}
+    on:change={onChangeAnimate}
+  >
+    <div bind:this={elem} class:noanimate={!animate} class:animate>
+      <slot />
+    </div>
   </div>
 </div>
 
 <style>
+.noanimate {
+  animation: none;
+}
 .animate {
 	animation-name: slide-in-bottom;
   animation-duration: 0.3s;
@@ -47,12 +74,9 @@
 @keyframes slide-in-bottom {
   0% {
     transform: translateY(300px);
-    opacity: 0;
   }
   100% {
     transform: translateY(0);
-    opacity: 1;
   }
 }
 </style>
-
