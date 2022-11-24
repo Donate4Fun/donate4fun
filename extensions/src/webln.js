2dcc5f538ef48e4a3c6d930c8bed6d4aba9c0e6b
Nikolay Bryskin <nbryskin@gmail.com>
2022-11-24 22:06:54 +0200

    [twitter] Fix reply auto-input in Twitter

--- extensions/src/webln.js
+++ extensions/src/webln.js
@@ -29,8 +29,12 @@ if (!window.donate4funPageScriptLoaded) {
 
   async function emulateKeypresses(selector, content) {
     const element = selector === ':focus' ? document.activeElement : document.querySelector(selector);
-    // Thanks to https://github.com/keepassxreboot/keepassxc-browser/blob/d7e34662637b869500e8bb6344cdd642c2fb079b/keepassxc-browser/content/keepassxc-browser.js#L659-L663
     // This code is here because if call it from content script warnings will occur "Permission denied to access property X". (YouTube overrides event handles and tries to access event attributes)
+    // This works on Chrome
+    const dataTransfer = new DataTransfer();
+    dataTransfer.setData("text/plain", content);
+    element.dispatchEvent(new ClipboardEvent('paste', { bubbles: true, clipboardData: dataTransfer, data: null }));
+    // And this works on Firefox
     element.dispatchEvent(new InputEvent('beforeinput', {bubbles: true, inputType: "insertText", data: content}));
     element.dispatchEvent(new InputEvent('input', {bubbles: true, inputType: "insertText", data: content}));
     element.dispatchEvent(new Event('change', {bubbles: true}));
