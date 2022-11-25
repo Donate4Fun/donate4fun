bd346ddc027584c86202fff98e901be528e2c4a8
Nikolay Bryskin <nbryskin@gmail.com>
2022-11-25 23:39:56 +0200

    [web] Add Telegram link in Footer

--- frontend/src/lib/Footer.svelte
+++ frontend/src/lib/Footer.svelte
@@ -2,6 +2,7 @@
   import { link } from "svelte-navigator";
   import GithubBrand from "svelte-awesome-icons/GithubBrand.svelte";
   import DiscordBrand from "svelte-awesome-icons/DiscordBrand.svelte";
+  import TelegramBrand from "svelte-awesome-icons/TelegramBrand.svelte";
   import TwitterBrand from "svelte-awesome-icons/TwitterBrand.svelte";
 
   const version = import.meta.env.VITE_VERSION;
@@ -11,6 +12,7 @@
   <div title={version}>2022 Donate4Fun<sup>©</sup></div>
   <div class="links">
     <a target=_blank href="https://twitter.com/donate4_fun"><TwitterBrand size=32 color="var(--link-color)" /></a>
+    <a target=_blank href="https://t.me/donate4fun"><TelegramBrand size=32 color="var(--link-color)" /></a>
     <a target=_blank href="https://github.com/donate4Fun/donate4fun"><GithubBrand size=32 color="var(--link-color)" /></a>
     <a target=_blank href="https://discord.gg/pfFRGWZ4TM"><DiscordBrand size=32 color="var(--link-color)" /></a>
     <a use:link href="/terms">Terms</a>
