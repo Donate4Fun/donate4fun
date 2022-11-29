8eda04fdd29a2e489a0472a9f2767260f9ca82db
Nikolay Bryskin <nbryskin@gmail.com>
2022-11-29 15:35:42 +0200

    fix(scripts): Update withdrawals stats scripts with new types of withdrawals

--- scripts/withdrawal-list.sh
+++ scripts/withdrawal-list.sh
@@ -1,8 +1,10 @@
 #!/bin/sh -e
 
 ./scripts/psql.sh -c '
-  select paid_at, amount, ytc.title
+  select paid_at, amount, ytc.title, twa.name, substring(don.lnauth_pubkey, 1, 7) as pubkey
   from withdrawal w
-  join youtube_channel ytc on w.youtube_channel_id = ytc.id
+  left join youtube_channel ytc on w.youtube_channel_id = ytc.id
+  left join twitter_author twa on w.twitter_author_id = twa.id
+  left join donator don on w.donator_id = don.id
   where paid_at is not null
   order by paid_at;'
--- scripts/withdrawals.sh
+++ scripts/withdrawals.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -e
 
 ./scripts/psql.sh -c 'copy (
-  select date(paid_at) as dt, amount
+  select date(paid_at) as dt, sum(amount) as amount
   from withdrawal w
-  join youtube_channel ytc on w.youtube_channel_id = ytc.id
   where paid_at is not null
+  group by dt
   order by dt
   ) to stdout;' | termgraph "$@"
