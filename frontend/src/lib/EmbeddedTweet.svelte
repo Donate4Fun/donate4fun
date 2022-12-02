5112b23ec6ca36d6ba841c9bbffe251590c5e00d
Nikolay Bryskin <nbryskin@gmail.com>
2022-12-02 18:32:22 +0200

    feat(twitter): Embed tweet in a donation page

---
+++ frontend/src/lib/EmbeddedTweet.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { onMount } from "svelte";
+
+  export let id;
+  let element;
+
+  onMount(async () => {
+    await twttr.widgets.createTweet(id.toString(), element);
+  });
+</script>
+
+<div bind:this={element}></div>
+
+<style>
+div {
+  width: 100%;
+}
+</style>
