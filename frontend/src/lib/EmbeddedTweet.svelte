c1395030018cf5344844ba50881154fe1eae72f9
Nikolay Bryskin <nbryskin@gmail.com>
2022-12-03 21:16:35 +0200

    feat(twitter): Add loader while Tweet preview loads

--- frontend/src/lib/EmbeddedTweet.svelte
+++ frontend/src/lib/EmbeddedTweet.svelte
@@ -1,18 +1,29 @@
 <script>
-  import { onMount } from "svelte";
+  import { tick } from "svelte";
+  import Loader from "$lib/Loader.svelte";
 
   export let id;
   let element;
 
-  onMount(async () => {
+  async function loadTweet() {
+    await tick();
+    console.log("elem", element);
     await twttr.widgets.createTweet(id.toString(), element);
-  });
+  }
 </script>
 
-<div bind:this={element}></div>
+<div bind:this={element}>
+  {#await loadTweet()}
+    <Loader />
+  {:then}
+  {/await}
+</div>
 
 <style>
 div {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   width: 100%;
 }
 </style>
