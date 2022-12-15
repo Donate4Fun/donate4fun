fb62d63729d7d13ed1c1de446e0882124c91065f
Nikolay Bryskin <nbryskin@gmail.com>
2022-12-15 20:18:46 +0200

    feat(web): Improve withdraw page (redirect back to profile)

--- frontend/src/routes/WithdrawPage.svelte
+++ frontend/src/routes/WithdrawPage.svelte
@@ -1,6 +1,8 @@
 <script>
   import { createEventDispatcher, onDestroy } from "svelte";
+  import { get } from "svelte/store";
   import { link, useResolve } from "svelte-navigator";
+
   import Loading from "$lib/Loading.svelte";
   import Donator from "$lib/Donator.svelte";
   import Amount from "$lib/Amount.svelte";
@@ -14,20 +16,17 @@
   import title from "$lib/title.js";
   import api from "$lib/api.js";
   import { notify } from "$lib/notifications.js";
+  import { me } from "$lib/session.js";
 
   export let navigate;
 
   let amount;
-  let loading = false;
-  let showSuccess = false;
-
-  const dispatch = createEventDispatcher();
-  const resolve = useResolve();
   let ws;
   onDestroy(() => ws?.close());
 
   async function load() {
     try {
+      const me_ = await get(me);
       const response = await api.get("me/withdraw");
       title.set(`Withdraw ${response.amount} sats`);
       ws = await api.subscribe(`withdrawal:${response.withdrawal_id}`);
@@ -35,8 +34,10 @@
         await ws.close();
         if (msg.status === 'ERROR') {
           notify(msg.message);
+        } else {
+          notify("Success", "Your funds were successefully withdrawed", "success");
+          navigate(`/donator/${me_.donator.id}`);
         }
-        showSuccess = true;
       });
       await ws.ready();
       return response;
@@ -52,29 +53,21 @@
   <div class="withdraw">
     {#await load() then { lnurl, amount }}
       <h1>Withdraw <Amount amount={amount} /></h1>
-      {#if showSuccess}
-        <img src="/static/success.png" class="success" alt="success">
-        <div class="donations-claimed">Donations claimed</div>
-        <div class="buttons success">
-          <GrayButton on:click={() => navigate(-1)}>Close</GrayButton>
-        </div>
-      {:else}
-          <div>Scan with your Lightning Wallet</div>
-          <a class="qrcode" href="lightning:{lnurl}"><QRCode value={lnurl} /></a>
-          <div class="buttons">
-            <a href="lightning:{lnurl}" class="open-in-wallet"><Button>Open in wallet</Button></a>
-            <Lnurl lnurl={lnurl} class="lnurl" />
-            <GrayButton on:click={() => navigate(-1)}>Cancel</GrayButton>
-          </div>
-          <div class="suggestion">
-            Don’t have a wallet? Download wallet and claim your donations with a Wallet Like
-            <a href="https://getalby.com" target="_blank">Alby</a>,
-            <a href="https://phoenix.acinq.co" target="_blank">Phoenix</a>,
-            <a href="https://sbw.app" target="_blank">SBW</a> or
-            <a href="https://blixtwallet.github.io" target="_blank">Blixt</a>
-            .
-          </div>
-      {/if}
+      <div>Scan with your Lightning Wallet</div>
+      <a class="qrcode" href="lightning:{lnurl}"><QRCode value={lnurl} /></a>
+      <div class="buttons">
+        <a href="lightning:{lnurl}" class="open-in-wallet"><Button>Open in wallet</Button></a>
+        <Lnurl lnurl={lnurl} class="lnurl" />
+        <GrayButton on:click={() => navigate(-1)}>Cancel</GrayButton>
+      </div>
+      <div class="suggestion">
+        Don’t have a wallet? Download wallet and claim your donations with a Wallet Like
+        <a href="https://getalby.com" target="_blank">Alby</a>,
+        <a href="https://phoenix.acinq.co" target="_blank">Phoenix</a>,
+        <a href="https://sbw.app" target="_blank">SBW</a> or
+        <a href="https://blixtwallet.github.io" target="_blank">Blixt</a>
+        .
+      </div>
     {/await}
   </div>
 </Section>
@@ -94,16 +87,6 @@ h1 {
   font-weight: 900;
   font-size: 24px;
 }
-img.success {
-  width: 120px;
-  height: 120px;
-}
-.donations-claimed {
-  margin-top: 20px;
-  margin-bottom: 14px;
-  font-weight: 900;
-  font-size: 24px;
-}
 .qrcode {
   margin-top: 16px;
 }
@@ -112,11 +95,6 @@ img.success {
   display: flex;
   flex-direction: column;
   align-items: center;
-}
-.buttons.success {
-  gap: 20px;
-  margin-top: 66px;
-  width: 400px;
 }
 .buttons .open-in-wallet {
   margin-top: 24px;
