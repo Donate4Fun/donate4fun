02e227e7690d9f7fb8b3b4f4282c02a53882fa91
Nikolay Bryskin <nbryskin@gmail.com>
2022-12-16 14:22:20 +0200

    chore: show errors in publish-chrome.sh

--- scripts/publish-chrome.sh
+++ scripts/publish-chrome.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-tokenfile=chrome-web-store-token.json
+tokenfile=secrets/chrome-web-store-token.json
 token=$(jq -r .access_token "$tokenfile")
 item_id=acckcppgcafhbdledejfiiaomafpjmgc
 
@@ -28,7 +28,14 @@ function publish() {
 function refresh_token() {
   local refresh_token=$(jq -r .refresh_token "$tokenfile")
   local url="https://accounts.google.com/o/oauth2/token?grant_type=refresh_token&client_id=$CHROME_WEB_STORE_CLIENT_ID&client_secret=$CHROME_WEB_STORE_CLIENT_SECRET&refresh_token=$refresh_token"
-  local access_token=$(curl -X POST "$url" | jq -r .access_token)
+  local body
+  body=$(curl --fail-with-body -X POST "$url")
+  local retval=$?
+  if [ $retval -ne 0 ]; then
+    echo "$body"
+    exit $retval
+  fi
+  local access_token=$(echo "$body" | jq -r .access_token)
   mv "$tokenfile" "$tokenfile.old"
   jq --arg token "$access_token" '.access_token = $token' "$tokenfile.old" > "$tokenfile"
 }
